6e822be81efc223d0e49dc2cf5cbef21008c36a4
Amol Patil <computer.warriors2020@gmail.com>
2017-10-24 18:25:09 +0530

    Update dropbatchdata.php

--- dropbatchdata.php
+++ dropbatchdata.php
@@ -1,16 +1,11 @@
 <?php
 		
-		//address of the server where db is installed
+		
 		$servername = "localhost";
-		//username to connect to the db
-		//the default value is root
 		$username = "root";
-		//password to connect to the db
-		//this is the value you would have specified during installation of WAMP stack
 		$password = "";
-		//name of the db under which the table is created
 		$dbName = "skala";
-		//establishing the connection to the db.
+
 		$conn = new mysqli($servername, $username, $password, $dbName);
 		//checking if there were any error during the last connection attempt
 		if ($conn->connect_error) {
