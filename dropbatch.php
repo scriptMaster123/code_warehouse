be8b2126a4f4bfb3f00f9f89301bf6033067ecc6
scriptMaster123 <33134919+scriptMaster123@users.noreply.github.com>
2017-10-27 01:30:02 +0530

    Update dropbatch.php

--- dropbatch.php
+++ dropbatch.php
@@ -7,6 +7,11 @@
 <html>
 <head>
 <title>Line chart</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="Institute performance analysis" />
    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
    <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
    <script type="text/javascript">
